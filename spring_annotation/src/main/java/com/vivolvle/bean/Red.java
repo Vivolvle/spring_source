d8380cc9dc3136ea96388eecb9729c0703443f39
weiliangzheng <1067310192@qq.com>
2019-10-20 20:55:40 +0800

    aware

---
+++ spring_annotation/src/main/java/com/vivolvle/bean/Red.java
@@ -0,0 +1,32 @@
+package com.vivolvle.bean;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.EmbeddedValueResolverAware;
+import org.springframework.util.StringValueResolver;
+
+/**
+ * @author weilz
+ * @date 2019/10/20
+ */
+public class Red implements ApplicationContextAware, BeanNameAware, EmbeddedValueResolverAware {
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("传入的ioc: "+applicationContext);
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        System.out.println("当前bean的名字: "+name);
+    }
+
+    @Override
+    public void setEmbeddedValueResolver(StringValueResolver resolver) {
+        String value = resolver.resolveStringValue("你好${os.name},值为#{18+5}");
+        System.out.println("解析的value值为: "+value);
+    }
+}
