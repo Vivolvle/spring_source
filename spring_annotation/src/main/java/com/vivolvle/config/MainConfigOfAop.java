220a5f26ebb9dd6b65377dabe98aa54d806abdcd
liangzheng.wei <liangzheng.wei@dingxiang-inc.com>
2019-10-31 19:27:23 +0800

    p31

--- spring_annotation/src/main/java/com/vivolvle/config/MainConfigOfAop.java
+++ spring_annotation/src/main/java/com/vivolvle/config/MainConfigOfAop.java
@@ -29,7 +29,35 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
  *      ->AspectJAwareAdvisorAutoProxyCreator
  *          ->AbstractAdvisorAutoProxyCreator
  *              ->AbstractAutoProxyCreator implements SmartInstantiationAwareBeanPostProcessor, BeanFactoryAware
- *              关注后置处理器（在bean初始化前后做事情）
+ *              关注后置处理器（在bean初始化前后做事情）,自动装配BeanFactory
+ *  AbstractAutoProxyCreator.setBeanFactory()
+ *  AbstractAutoProxyCreator 有后置处理器的逻辑
+ *
+ *  AbstractAdvisorAutoProxyCreator.setBeanFactory()->initBeanFactory()
+ *
+ *  AnnotationAwareAspectJAutoProxyCreator.initBeanFactory()
+ *
+ *  流程:
+ *      1)：传入配置类，创建ioc容器
+ *      2：注册配置类，调用refresh()刷新容器
+ *      3：registerBeanPostProcessors(beanFactory);注册bean的后置处理器来方便拦截bean的创建
+ *          1）：先获取ioc容器已经定义了的需要创建对象的所有BeanPostProcessor
+ *          2）：给容器中加别的BeanPostProcessor
+ *          3）：优先注册实现了PriorityOrdered接口的BeanPostProcessor
+ *          4）：在给容器中注册实现了Ordered接口的BeanPostProcessor
+ *          5）：注册没有实现优先级接口的BeanPostProcessor
+ *          6）：注册BeanPostProcessor，实际上就是创建BeanPostProcessor对象，保存在容器中
+ *              创建internalAutoProxyCreator的BeanPostProcessor[AnnotationAwareAspectJAutoProxyCreator]
+ *              1).创建Bean的实例
+ *              2),populateBean(beanName,mbd,instanceWrapper);bean的各种属性赋值
+ *              3),initializeBean，初始化bean
+ *                  1）：invokeAwareMethods(),处理Aware接口的方法回调
+ *                  2）：applyBeanPostProcessorsBeforeInitialization():应用后置处理器的BeforeInitialization
+ *                  3）：invokeInitMethods():执行自定义的初始化的方法
+ *                  4）：applyBeanPostProcessorsAfterInitialization():应用后置处理器的postProcessAfterInitialization
+ *              4):BeanPostProcessor(AnnotationAwareAspectJAutoProxyCreator)创建成功;-->aspectJAdvisorsBuilder
+ *          7)：把BeanPostProcessor注册到BeanFactory中
+ *====================以上是创建和注册AnnotationAwareAspectJAutoProxyCreator的过程===================================
  * @see MainConfig2 同样使用了ImportBeanDefinitionRegistrar
  * @author weilz
  * @date 2019/10/26
